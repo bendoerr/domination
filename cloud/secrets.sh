69edd0d75ab4cbfdd1db394efca180bda3f8cb99
Ben Doerr <craftsman@bendoerr.me>
2017-06-18 11:46:30 -0400

    Cleanup secrets script

--- cloud/secrets.sh
+++ cloud/secrets.sh
@@ -1,36 +1,40 @@
 ################################################################################
 # Standard Constants
 
-# Script Invocation Info
-readonly SCRIPT_INVOKE="$0"
-readonly SCRIPT_NAME=`basename "$0"`
-readonly SCRIPT_ABS_PATH="$( [[ "${SCRIPT_INVOKE:0:1}" == "/" ]] \
-                                && dirname "$SCRIPT_INVOKE" \
-                                || dirname "$(pwd)/$SCRIPT_INVOKE" )"
+function main() {
+    # Script Invocation Info
+    local -r SCRIPT_INVOKE="$0"
+    local -r SCRIPT_NAME=`basename "$0"`
+    local -r SCRIPT_ABS_PATH="$( [[ "${SCRIPT_INVOKE:0:1}" == "/" ]] \
+                                    && dirname "$SCRIPT_INVOKE" \
+                                    || dirname "$(pwd)/$SCRIPT_INVOKE" )"
 
 
-################################################################################
-# Script Variables
+    ################################################################################
+    # Script Variables
 
-readonly OPENSSL_BIN="/usr/local/opt/openssl/bin/openssl"
-readonly OPENSSL_DEC=(
-                     "enc"
-                     "-aes-256-cbc"
-                     "-d"
-                     "-base64"
-                     "-in"
-                     )
+    local -r OPENSSL_BIN="/usr/local/opt/openssl/bin/openssl"
+    local -r OPENSSL_DEC=(
+                        "enc"
+                        "-aes-256-cbc"
+                        "-d"
+                        "-base64"
+                        "-in"
+                        )
 
-readonly DIGITALOCEAN_TOKEN_SECRET_FILE="$SCRIPT_ABS_PATH/.secrets.digitalocean_token.enc"
+    local -r DIGITALOCEAN_TOKEN_SECRET_FILE="$SCRIPT_ABS_PATH/.secrets.digitalocean_token.enc"
 
-################################################################################
-# BUSINESS
+    ################################################################################
+    # BUSINESS
 
 
-echo -n "[secrets.digitalocean_token] "
-readonly do_token="$(${OPENSSL_BIN} ${OPENSSL_DEC[@]} "${DIGITALOCEAN_TOKEN_SECRET_FILE}")"
+    echo -n "[secrets.digitalocean_token] "
+    local -r do_token="$(${OPENSSL_BIN} ${OPENSSL_DEC[@]} "${DIGITALOCEAN_TOKEN_SECRET_FILE}")"
 
-# For Terraform
-export DIGITALOCEAN_TOKEN="${do_token}"
-# For Ansible
-export DO_API_TOKEN="${do_token}"
+    # For Terraform
+    export DIGITALOCEAN_TOKEN="${do_token}"
+    # For Ansible
+    export DO_API_TOKEN="${do_token}"
+}
+
+main $0
